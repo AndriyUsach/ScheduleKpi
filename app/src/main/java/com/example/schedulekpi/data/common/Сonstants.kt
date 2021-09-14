95fa930a6e329b2a7cacbd162ef48b4479ce3168
AndriyUsach <andryhausach@gmail.com>
2021-09-14 14:03:49 +0300

    create DataStore with Preferences

--- app/src/main/java/com/example/schedulekpi/data/common/Сonstants.kt
+++ app/src/main/java/com/example/schedulekpi/data/common/Сonstants.kt
@@ -1,5 +1,10 @@
 package com.example.schedulekpi.data.common
 
+import androidx.datastore.preferences.core.stringPreferencesKey
+
 object Constants {
     const val BASE_URL = "http://api.rozklad.org.ua/v2/"
+    const val DATA_STORE_NAME = "schedule_settings"
+    val DATA_STORE_SCHEDULE_TYPE = stringPreferencesKey("data_store_schedule_type")
+    val DATA_STORE_SCHEDULE_OWNER = stringPreferencesKey("data_store_schedule_owner")
 }
--- app/src/main/java/com/example/schedulekpi/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/schedulekpi/ui/activity/MainActivity.kt
@@ -11,34 +11,46 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.lifecycleScope
 import com.example.schedulekpi.app.ScheduleApplication
+import com.example.schedulekpi.data.common.Constants
 import com.example.schedulekpi.ui.UiState
 import com.example.schedulekpi.ui.screen.SplashScreen
 import com.example.schedulekpi.ui.theme.ScheduleKpiTheme
+import com.example.schedulekpi.utils.dataStore
 import com.example.schedulekpi.viewModel.MainViewModel
 import com.example.schedulekpi.viewModel.MainViewModelFactory
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 
 class MainActivity : ComponentActivity() {
-    private val uiStateViewModel: MainViewModel by lazy {
-        MainViewModelFactory().create(MainViewModel::class.java)
+    private val mainViewModel: MainViewModel by lazy {
+        MainViewModelFactory(this.dataStore).create(MainViewModel::class.java)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ScheduleApplication.instance.applicationComponent.inject(this)
 
+
         lifecycleScope.launchWhenStarted {
             delay(3000)
-            uiStateViewModel.setNewUiState(UiState.MainScreenState)
+            val scheduleInfo = mainViewModel.readGroupScheduleInfoFromDataStore()
+            val uiState = selectNextUiState(scheduleInfo)
+            mainViewModel.setNewUiState(uiState)
         }
 
         setContent {
             ScheduleKpiTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background, modifier = Modifier.fillMaxSize()) {
-                    uiStateViewModel.uiState.collectAsState().value.let { uiState ->
+                    mainViewModel.uiState.collectAsState().value.let { uiState ->
                         when(uiState) {
                             is UiState.SplashState -> SplashScreen()
+                            is UiState.ScheduleSelectorState -> {
+                                Text(text = "This is ScheduleSelectorState")
+                            }
                             is UiState.MainScreenState -> {
                                 Text(text = "Hello Android")
                             }
@@ -48,5 +60,11 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+    
+    private fun selectNextUiState(scheduleInfo: Pair<String?, String?>): UiState {
+        if (scheduleInfo.first.isNullOrEmpty() || scheduleInfo.second.isNullOrEmpty())
+            return UiState.ScheduleSelectorState
+        return UiState.MainScreenState
     }
 }
